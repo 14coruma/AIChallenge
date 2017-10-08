e0dea913ffa4d349f7682731557eb3f828c16638
Andrew Corum <14coruma@gmail.com>
2017-10-07 21:06:41 -0400

    * client: Submit form using client.js

--- client/js/client.js
+++ client/js/client.js
@@ -3,5 +3,12 @@
 var startButton = document.getElementById( 'startButton' );
 
 startButton.addEventListener( 'click', function() {
-	alert("HI");
+	var username = document.getElementById( 'formUsername' ).value;
+	var password = document.getElementById( 'formPassword' ).value;
+	var game     = document.getElementById( 'selectGame' ).value;
+
+	var xhr = new XMLHttpRequest();
+	xhr.open( "POST", 'http://localhost:2306/signup', true );
+	xhr.setRequestHeader( "Content-Type", "application/x-www-form-urlencoded" );
+	xhr.send( "username="+username+"&password="+password+"&email="+game );
 });
