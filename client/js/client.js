ec9547b36d518a4eaff508191b084285b2d1bebb
Andrew Corum <14coruma@gmail.com>
2017-10-08 19:30:55 -0400

    * client: Send post to server

--- client/js/client.js
+++ client/js/client.js
@@ -8,7 +8,15 @@ startButton.addEventListener( 'click', function() {
 	var game     = document.getElementById( 'selectGame' ).value;
 
 	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+		if ( xhr.readyState ==4 && xhr.status == 200) {
+			return xhr.responseText;
+		}
+	}
+
 	xhr.open( "POST", 'http://localhost:2306/signup', true );
 	xhr.setRequestHeader( "Content-Type", "application/x-www-form-urlencoded" );
 	xhr.send( "username="+username+"&password="+password+"&email="+game );
+
+	location.replace( "html/waiting.html" );
 });
