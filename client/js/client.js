3eada5ffba0d2d1ffc3ff47555f02b83c27b3457
Andrew Corum <14coruma@gmail.com>
2017-10-08 20:34:55 -0400

    - client: actually wait now :)

--- client/js/client.js
+++ client/js/client.js
@@ -7,19 +7,23 @@ startButton.addEventListener( 'click', function() {
 	var password = document.getElementById( 'formPassword' ).value;
 	var game     = document.getElementById( 'selectGame' ).value;
 
-	document.getElementById( 'waitingMessage' ).style.display="block";
+	if ( username && password && game ) {
+		var waitMsg = document.getElementById( 'waitingMessage' );
+		waitMsg.style.display="block";
 
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function() {
-		if ( xhr.readyState ==4 && xhr.status == 200) {
-			return xhr.responseText;
+		var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+			if ( xhr.readyState ==4 && xhr.status == 200) {
+				waitMsg.style.display="none";
+				return xhr.responseText;
+			}
 		}
+
+		xhr.open( "POST", 'http://localhost:2306/signup', true );
+		xhr.setRequestHeader( "Content-Type", "application/x-www-form-urlencoded" );
+		xhr.send( "username="+username+"&password="+password+"&email="+game );
+
+		//location.replace( "html/waiting.html" );
+		return false;
 	}
-
-	xhr.open( "POST", 'http://localhost:2306/signup', true );
-	xhr.setRequestHeader( "Content-Type", "application/x-www-form-urlencoded" );
-	xhr.send( "username="+username+"&password="+password+"&email="+game );
-
-	//location.replace( "html/waiting.html" );
-	return false;
 } );
