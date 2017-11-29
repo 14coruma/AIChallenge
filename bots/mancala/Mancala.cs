e1609525a55b795bcb02698b2972b3a6dea2a29a
Andrew Corum <14coruma@gmail.com>
2017-11-29 01:40:56 -0500

    * Get Mancala.cs to work with server

--- bots/mancala/Mancala.cs
+++ bots/mancala/Mancala.cs
@@ -12,14 +12,16 @@ namespace Mankalah
 
 		public static void Main(String[] args)
 		{
+			//String input = args[0].Substring(1, args[0].Length - 2);
+			//dynamic json = JObject.Parse(input);
 			dynamic json = JObject.Parse(args[0]);
 			Board board;
-			if (json.currentPlayer == 0)
+			if (json.state.currentPlayer == 1)
 				board = new Board(Position.Top);
 			else
 				board = new Board(Position.Bottom);
 			for (int i = 0; i < 14; i++) {
-				board.board[i] = json.board[i];
+				board.board[i] = json.state.board[i];
 			}
 			int move = bot.chooseMove(board);
 			Console.WriteLine(move);
