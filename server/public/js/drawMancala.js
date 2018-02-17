ead8ac07e79a922d6f6d3fb5d302f5c2278fd3be
Andrew Corum <14coruma@gmail.com>
2018-02-17 15:25:04 -0500

    * mancala: Add ineractive graphics for mancala

--- server/public/js/drawMancala.js
+++ server/public/js/drawMancala.js
@@ -5,59 +5,85 @@
  */
 
 'use strict';
-var canvas;
-var ctx;
+var canvas = document.getElementById( "myCanvas" ),
+    ctx = canvas.getContext( "2d" ),
+    canvasLeft = canvas.offsetLeft,
+    canvasTop = canvas.offsetTop,
+    elements = [],
+    move = -1;
 
 function drawMancala( state ) {
-	canvas = document.getElementById( "myCanvas" );
-	ctx = canvas.getContext( "2d" );
 	ctx.clearRect( 0, 0, canvas.width, canvas.height );
+	elements = [];
 	ctx.font = "18px Arial";
 	ctx.textAlign = "left";
 	ctx.fillText( "P1: " + state.players[0].username, 10, canvas.height - 32 );
 	ctx.fillText( "P2: " + state.players[1].username, 10, 32 );
 
 	let spacer = ( canvas.width - 64 ) / 8;
+	ctx.textAlign = "center";
 	// Draw bottom circles
 	for ( var i = 0; i < 6; i++ ) {
 		var x = 64 + spacer * ( i + 1 );
 		var y = 2 * canvas.height / 3;
-		drawCircle( x, y, 24 );
+		if ( move == i ) {
+			drawCircle( x, y, 24, "GreenYellow", "GoldenRod" );
+		} else {
+			drawCircle( x, y, 24, "GreenYellow", "Green" );
+		}
 		ctx.fillText( state.board[i], x, y );
+		elements.push({
+			id: i,
+			width: 48,
+			height: 48,
+			top: y - 24,
+			left: x - 24,
+		});
 	}
 	// Draw top circles
 	for ( var i = 7; i < 13; i++ ) {
 		var x = canvas.width - 64 - spacer * ( i - 6 );
 		var y = canvas.height / 3;
-		drawCircle( x, y, 24 );
+		if ( move == i ) {
+			drawCircle( x, y, 24, "GreenYellow", "GoldenRod" );
+		} else {
+			drawCircle( x, y, 24, "GreenYellow", "Green" );
+		}
 		ctx.fillText( state.board[i], x, y );
+		elements.push({
+			id: i,
+			width: 48,
+			height: 48,
+			top: y - 24,
+			left: x - 24
+		});
 	}
-	drawOval( 24, canvas.height / 2, 24 );
-	ctx.fillText( state.board[13], 24, canvas.height / 2 );
-	drawOval( canvas.width - 24, canvas.height / 2, 24 );
-	ctx.fillText( state.board[6], canvas.width - 24, canvas.height / 2 );
+	// Draw goals
+	drawOval( 32, canvas.height / 2, 24, "GreenYellow", "Green" );
+	ctx.fillText( state.board[13], 32, canvas.height / 2 );
+	drawOval( canvas.width - 32, canvas.height / 2, 24, "GreenYellow", "Green" );
+	ctx.fillText( state.board[6], canvas.width - 32, canvas.height / 2 );
 
 	if (state.gameOver == 1) {
 		ctx.font = "30px Arial";
-		ctx.textAlign = "center";
 		ctx.fillText( "Game Over", canvas.width / 2, canvas.height / 4 );
 		ctx.fillText( state.players[state.winner].username + " Wins!", canvas.width / 2, canvas.height / 2);
 	}
 }
 
-function drawCircle( x, y, r ) {
+function drawCircle( x, y, r, color1, color2 ) {
 		ctx.save();
 		ctx.beginPath();
 		ctx.arc( x, y, r, 0, 2 * Math.PI, false);
-		ctx.fillStyle = 'green';
+		ctx.fillStyle = color1;
 		ctx.fill();
 		ctx.lineWidth = 5;
-		ctx.strokeStyle = "#003300";
+		ctx.strokeStyle = color2;
 		ctx.stroke();
 		ctx.restore();
 }
 
-function drawOval( x, y, r ) {
+function drawOval( x, y, r, color1, color2 ) {
 		ctx.save();
 		ctx.translate( x, y );
 		ctx.scale( 1, 2 );
@@ -65,10 +91,24 @@ function drawOval( x, y, r ) {
 		ctx.arc( 0, 0, r, 0, 2 * Math.PI, false);
 		ctx.restore();
 		ctx.save();
-		ctx.fillStyle = 'green';
+		ctx.fillStyle = color1;
 		ctx.fill();
 		ctx.lineWidth = 5;
-		ctx.strokeStyle = "#003300";
+		ctx.strokeStyle = color2;
 		ctx.stroke();
 		ctx.restore();
 }
+
+canvas.addEventListener( 'click', function( ev ) {
+	var x = ev.pageX - canvasLeft,
+	    y = ev.pageY - canvasTop;
+
+	// Check for element clicked
+	elements.forEach( function( element ) {
+		if ( y > element.top && y < element.top + element.height &&
+			x > element.left && x < element.left + element.width ) {
+			move = element.id;
+			document.getElementById( "formMove" ).value = move;
+		}
+	});
+}, false );
